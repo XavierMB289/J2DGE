4e98166648dce3ff35587c97c6031398c5485372
XavierMB289 <48699746+XavierMB289@users.noreply.github.com>
2023-04-10 23:48:15 -0400

    Delete src/online directory

--- src/online/CustomParser.java
+++
@@ -1,9 +0,0 @@
-package online;
-
-import java.nio.channels.SocketChannel;
-
-public abstract class CustomParser {
-	
-	public abstract void parse(SocketChannel client, String input);
-		
-}
--- src/online/ServerData.java
+++
@@ -1,7 +0,0 @@
-package online;
-
-import java.io.PrintWriter;
-
-public abstract class ServerData {
-	public abstract void doRequest(String line, PrintWriter out);
-}
